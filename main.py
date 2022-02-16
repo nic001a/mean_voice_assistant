a5d7bef7b28f1b0d35eed7e57a17a4afd662d72f
nic001a <nicola.info.dimarco@gmail.com>
2022-02-16 16:31:27 +0100

    awsome

---
+++ main.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import txttospch
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.pause_threshold = 1
+    r.adjust_for_ambient_noise(source)
+    print("Listening...")
+    audio = r.listen(source)
+try:
+    print("Recognizing...")
+    query = r.recognize_google(audio, language='en-in')
+    print(f"user said: {query}\n")
+
+    if 'weather' in query:
+        txttospch.weather_response()
+
+    txttospch.curse_response()
+except Exception as e:
+    print("BRO STFU")
