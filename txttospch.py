0f9d33ead50c7c0fe0faa0671cecaa6402a6c22e
nic001a <nicola.info.dimarco@gmail.com>
2022-02-18 13:09:14 +0100

    needs more feqtures but as a prrof of concept its alright

--- txttospch.py
+++ txttospch.py
@@ -5,7 +5,7 @@ import csv
 
 def weather_response():
     tts = gTTS(
-        text="Are you dumb? check the weather for yourself", lang="en")
+        text="HUmanity has spent so many years developing internet machine learning and programming languages and the first you can think about is to ask about the weather ? stick your head out of the window and shut up!", lang="en")
     tts.save("weather.mp3")
     playsound("weather.mp3")
 
@@ -15,7 +15,7 @@ def weather_response():
 
 def small_pp():
     tts = gTTS(
-        text="Fuck you dumbass I am a super smart robot and I'll come for you", lang="en")
+        text="More real than you", lang="en")
     tts.save("stupid.mp3")
     playsound("stupid.mp3")
 
@@ -25,3 +25,10 @@ def curse_response(word_here):
         file_read = csv.reader(csv_file)
         if word_here in csv_file:
             small_pp()
+
+
+def roast():
+    tts = gTTS(
+    text="Bob the builder took one look at you and said we cant fix that!", lang="en")
+    tts.save("stupid.mp3")
+    playsound("stupid.mp3")
